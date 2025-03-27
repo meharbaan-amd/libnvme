bba499ddd9360fdb329dbca3343391b2a197d60c
Meharbaan Ali <meharbaan.ali@amd.com>
2025-03-27 06:50:47 +0000

    update, detach, delete ns

--- examples/mi-mctp.c
+++ examples/mi-mctp.c
@@ -757,6 +757,7 @@ typedef enum breck_nvme_cmd_opcodes_e {
 	BRECK_NVME_CMD_ATTACH_NAMESPACE = 0xD4,
 	BRECK_NVME_CMD_DETACH_NAMESPACE = 0xD8,
 	BRECK_NVME_CMD_DELETE_NAMESPACE = 0xE0,
+	BRECK_NVME_CMD_UPDATE_NAMESPACE_ATTR = 0xDC,
 	BRECK_NVME_CMD_SET_FEATURES = 0x09,
 	BRECK_NVME_CMD_GET_FEATURES = 0x0A,
 	BRECK_NVME_CMD_GET_LOG_PAGE = 0x02,
@@ -800,6 +801,7 @@ nvme_cmd_str(breck_nvme_cmd_opcodes_t type)
     A2STR(ATTACH_NAMESPACE);
     A2STR(DETACH_NAMESPACE);
     A2STR(DELETE_NAMESPACE);
+    A2STR(UPDATE_NAMESPACE_ATTR);
     A2STR(GET_LOG_PAGE);
   }
 #undef A2STR
@@ -1140,6 +1142,135 @@ int do_attach_ns_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_
         return 0;
 }
 
+int do_detach_ns_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_t* resp_data)
+{
+        struct nvme_mi_admin_resp_hdr *resp;
+        struct nvme_mi_admin_req_hdr *req;
+        uint8_t resp_buf[512] = {0};
+        uint8_t req_buf[512] = {0};
+        struct nvme_mi_ctrl *ctrl;
+        size_t resp_data_len = 0;
+        size_t req_data_len;
+        uint16_t ctrl_id;
+        int rc;
+        ctrl_id = 0; /* Management Controller */
+
+        req = (struct nvme_mi_admin_req_hdr *)req_buf;
+        resp = (struct nvme_mi_admin_resp_hdr *)resp_buf;
+
+        memcpy(&req->opcode, req_data->nvme_cmd, sizeof(struct nvme_mi_admin_req_hdr) + req_data->payload_len);
+        req_data_len = req_data->payload_len;
+        req->ctrl_id = cpu_to_le16(ctrl_id);
+
+	dump_req(nvme_cmd_str(BRECK_NVME_CMD_DETACH_NAMESPACE), req, req_buf, req_data_len);
+
+        ctrl = nvme_mi_init_ctrl(ep, ctrl_id);
+        if (!ctrl) {
+                warn("can't create controller");
+                return -1;
+        }
+
+        printf(" req_data_len: %lu\n", req_data_len);
+
+        rc = nvme_mi_admin_xfer(ctrl, req, req_data_len, resp, 0, &resp_data_len);
+        if (rc) {
+                warn("nvme_admin_xfer failed: %d", rc);
+                return -1;
+        }
+
+        memcpy(resp_data, resp_buf, sizeof(struct nvme_mi_admin_resp_hdr) + resp_data_len);
+        resp_data->payload_len = resp_data_len;
+	dump_resp(nvme_cmd_str(BRECK_NVME_CMD_DETACH_NAMESPACE), resp, resp_buf, resp_data_len);
+
+        return 0;
+}
+
+int do_delete_ns_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_t* resp_data)
+{
+        struct nvme_mi_admin_resp_hdr *resp;
+        struct nvme_mi_admin_req_hdr *req;
+        uint8_t resp_buf[512] = {0};
+        uint8_t req_buf[512] = {0};
+        struct nvme_mi_ctrl *ctrl;
+        size_t resp_data_len = 0;
+        size_t req_data_len;
+        uint16_t ctrl_id;
+        int rc;
+        ctrl_id = 0; /* Management Controller */
+
+        req = (struct nvme_mi_admin_req_hdr *)req_buf;
+        resp = (struct nvme_mi_admin_resp_hdr *)resp_buf;
+
+        memcpy(&req->opcode, req_data->nvme_cmd, sizeof(struct nvme_mi_admin_req_hdr) + req_data->payload_len);
+        req_data_len = req_data->payload_len;
+        req->ctrl_id = cpu_to_le16(ctrl_id);
+
+	dump_req(nvme_cmd_str(BRECK_NVME_CMD_DELETE_NAMESPACE), req, req_buf, req_data_len);
+
+        ctrl = nvme_mi_init_ctrl(ep, ctrl_id);
+        if (!ctrl) {
+                warn("can't create controller");
+                return -1;
+        }
+
+        printf(" req_data_len: %lu\n", req_data_len);
+
+        rc = nvme_mi_admin_xfer(ctrl, req, req_data_len, resp, 0, &resp_data_len);
+        if (rc) {
+                warn("nvme_admin_xfer failed: %d", rc);
+                return -1;
+        }
+
+        memcpy(resp_data, resp_buf, sizeof(struct nvme_mi_admin_resp_hdr) + resp_data_len);
+        resp_data->payload_len = resp_data_len;
+	dump_resp(nvme_cmd_str(BRECK_NVME_CMD_DELETE_NAMESPACE), resp, resp_buf, resp_data_len);
+
+        return 0;
+}
+
+int do_update_ns_attr_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_t* resp_data)
+{
+        struct nvme_mi_admin_resp_hdr *resp;
+        struct nvme_mi_admin_req_hdr *req;
+        uint8_t resp_buf[512] = {0};
+        uint8_t req_buf[512] = {0};
+        struct nvme_mi_ctrl *ctrl;
+        size_t resp_data_len = 0;
+        size_t req_data_len;
+        uint16_t ctrl_id;
+        int rc;
+        ctrl_id = 0; /* Management Controller */
+
+        req = (struct nvme_mi_admin_req_hdr *)req_buf;
+        resp = (struct nvme_mi_admin_resp_hdr *)resp_buf;
+
+        memcpy(&req->opcode, req_data->nvme_cmd, sizeof(struct nvme_mi_admin_req_hdr) + req_data->payload_len);
+        req_data_len = req_data->payload_len;
+        req->ctrl_id = cpu_to_le16(ctrl_id);
+
+	dump_req(nvme_cmd_str(BRECK_NVME_CMD_UPDATE_NAMESPACE_ATTR), req, req_buf, req_data_len);
+
+        ctrl = nvme_mi_init_ctrl(ep, ctrl_id);
+        if (!ctrl) {
+                warn("can't create controller");
+                return -1;
+        }
+
+        printf(" req_data_len: %lu\n", req_data_len);
+
+        rc = nvme_mi_admin_xfer(ctrl, req, req_data_len, resp, 0, &resp_data_len);
+        if (rc) {
+                warn("nvme_admin_xfer failed: %d", rc);
+                return -1;
+        }
+
+        memcpy(resp_data, resp_buf, sizeof(struct nvme_mi_admin_resp_hdr) + resp_data_len);
+        resp_data->payload_len = resp_data_len;
+	dump_resp(nvme_cmd_str(BRECK_NVME_CMD_UPDATE_NAMESPACE_ATTR), resp, resp_buf, resp_data_len);
+
+        return 0;
+}
+
 int do_get_log_page_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_t* resp_data)
 {
         struct nvme_mi_admin_resp_hdr *resp;
@@ -1209,12 +1340,21 @@ int do_admin_raw_breck(nvme_mi_ep_t ep, cmd_mvni_req_t* req_data, cmd_mvni_resp_
 		case BRECK_NVME_CMD_ATTACH_NAMESPACE:
 			do_attach_ns_breck(ep, req_data, resp_data);
 			break;
+		case BRECK_NVME_CMD_DETACH_NAMESPACE:
+			do_detach_ns_breck(ep, req_data, resp_data);
+			break;
+		case BRECK_NVME_CMD_DELETE_NAMESPACE:
+			do_delete_ns_breck(ep, req_data, resp_data);
+			break;
+		case BRECK_NVME_CMD_UPDATE_NAMESPACE_ATTR:
+			do_update_ns_attr_breck(ep, req_data, resp_data);
+			break;
 		case BRECK_NVME_CMD_GET_LOG_PAGE:
 			do_get_log_page_breck(ep, req_data, resp_data);
 			break;
 		default:
 			printf("Breck cmd opcode not supported: 0x%02x.", req.opcode);
-			return 0;
+			return -1;
 	}
 
         return 0;
